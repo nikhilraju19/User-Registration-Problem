1a5a5250d76713bb617fc6525c7c007493af292d
Your Name <nikhilvardhanraju@gmail.com>
2025-02-10 20:14:28 +0530

    [Nikhil] Add: added pyhton script to check for a valid email id

--- user.py
+++ user.py
@@ -10,31 +10,44 @@ logging.basicConfig(
 		logging.StreamHandler()
 	])
 
+def email_id():
+    email = input("Enter your email id: ")
+    pattern = r'^[a-zA-Z0-9+%_-]+(?:\.[a-zA-Z0-9+%_-]+)?@[a-zA-Z0-9]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$'
+    while True:
+        search = re.fullmatch(pattern, email)
+        if search:
+            logging.info(f"{email} is valid email id")
+            print(f"{email} is a valid email id")
+            return email
+        else:
+            logging.info(f"{email} is valid email id")
+            print("Please enter a valid email id")
+
 def last_name():
     pattern = r'^[A-Z][a-z]{2,}$'
     while True:
-        last_name = input("Enter your last name: ")
-        search = re.fullmatch(pattern,last_name)
+        valid_last_name = input("Enter your last name: ")
+        search = re.fullmatch(pattern,valid_last_name)
         if search:
-            logging.info(f"{last_name} is a valid last name")
-            print(f"{last_name} is a valid last name")
-            return last_name
+            logging.info(f"{valid_last_name} is a valid last name")
+            return valid_last_name
         else:
-            logging.warning(f"{last_name} is a invalid last name. Please enter a valid last name")
-            print("Please enter  valid last name")
+            logging.warning(f"{valid_last_name} is a invalid last name. Please enter a valid last name")
+            print("Please enter valid last name")
 
 def first_name():
     pattern = r'^[A-Z][a-z]{2,}(?: [A-Z][a-z]+){0,2}$'
     while True:
-        first_name = input("Enter your first name: ")
-        search = re.fullmatch(pattern,first_name)
+        valid_first_name = input("Enter your first name: ")
+        search = re.fullmatch(pattern,valid_first_name)
         if search:
-            logging.info(f"{first_name} is a valid first name")
-            return first_name
+            logging.info(f"{valid_first_name} is a valid first name")
+            return valid_first_name
         else:
-            logging.warning(f"{first_name} is a invalid first name. Please enter a valid first name")
-            print("Please enter  valid first name")
+            logging.warning(f"{valid_first_name} is a invalid first name. Please enter a valid first name")
+            print("Please enter valid first name")
 
 if __name__ == "__main__":
     first_name()
     last_name()
+    email_id()
