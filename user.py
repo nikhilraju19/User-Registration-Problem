36c70842537019f1c6007ad57752b2df331e7177
Your Name <nikhilvardhanraju@gmail.com>
2025-02-17 00:11:27 +0530

    [Nikhil] Refactor: Maintained code hygiene and refactored the code.

--- user.py
+++ user.py
@@ -10,18 +10,39 @@ logging.basicConfig(
 		logging.StreamHandler()
 	])
 
-def first_name():
+def validate_first_name(first_name):
+    """
+	Description:
+		Checks whether first name is valid or not
+	Parameters:
+		first_name: first name to validate
+	Return:
+		bool: True if first name is valid, False otherwise
+    """
     pattern = r'^[A-Z][a-z]{2,}(?: [A-Z][a-z]+){0,2}$'
+    search = re.fullmatch(pattern,first_name)
+    if search:
+        logging.info(f"{first_name} is a valid first name")
+        return True
+    else:
+        logging.warning(f"{first_name} is an invalid first name.")
+        return False
+            
+def main():
+    """
+	Description:
+		Collects user input and validates using necessary sub-functions.
+	Parameters:
+		None
+	Return:
+		None
+    """
     while True:
-        first_name = input("Enter your first name: ")
-        search = re.search(pattern,first_name)
-        if search:
-            logging.info(f"{first_name} is a valid first name")
-            print(f"{first_name} is a valid first name")
-            return first_name
-        else:
-            logging.warning(f"{first_name} is a invalid first name. Please enter a valid first name")
-            print("Please enter  valid first name")
-
+        first = input("Enter your first name: ")
+        if validate_first_name(first):
+            print(f"{first} is a valid first name")
+            break
+        print("Please enter  valid first name")
+        
 if __name__ == "__main__":
-    first_name()
+    main()  
