8455aba1da0b2c979f9e942f21162b3ca74177aa
Your Name <nikhilvardhanraju@gmail.com>
2025-02-17 01:17:44 +0530

    [Nikhil] Refactor: Maintained code hygiene and refactored the code.

--- user.py
+++ user.py
@@ -10,70 +10,131 @@ logging.basicConfig(
 		logging.StreamHandler()
 	])
 
-def password():
+def validate_first_name(first_name):
+    """
+	Description:
+		Checks whether first name is valid or not
+	Parameters:
+		first_name: first name to validate
+	Return:
+		bool: True if first name is valid, False otherwise
+    """
+    pattern = r'^[A-Z][a-z]{2,}(?: [A-Z][a-z]+){0,2}$'
+    search = re.fullmatch(pattern,first_name)
+    if search:
+        logging.info(f"{first_name} is a valid first name")
+        return True
+    else:
+        logging.warning(f"{first_name} is an invalid first name.")
+        return False
+    
+def validate_last_name(last_name):
+    """
+	Description:
+		Checks whether last name is valid or not
+	Parameters:
+		last_name: last name to validate
+	Return:
+		bool: True if last name is valid, False otherwise
+    """
+    pattern = r'^[A-Z][a-z]{2,}$'
+    search = re.fullmatch(pattern,last_name)
+    if search:
+        logging.info(f"{last_name} is a valid last name")
+        return True
+    else:
+        logging.warning(f"{last_name} is an invalid last name.")
+        return False
+
+def validate_email_id(email_id):
+    """
+	Description:
+		Checks whether email id is valid or not
+	Parameters:
+		email_id: email id to validate
+	Return:
+		bool: True if email id is valid, False otherwise
+    """
+    pattern = r'^[a-zA-Z0-9+%_-]+(?:\.[a-zA-Z0-9+%_-]+)?@[a-zA-Z0-9]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$'
+    search = re.fullmatch(pattern, email_id)
+    if search:
+        logging.info(f"{email_id} is valid email id")
+        return True
+    else:
+        logging.warning(f"{email_id} is invalid email id")
+        return False
+
+def validate_mobile_number(mobile_number):
+    """
+	Description:
+		Checks whether mobile number is valid or not
+	Parameters:
+		mobile_number: mobile number to validate
+	Return:
+		bool: True if mobile number is valid, False otherwise
+    """
+    pattern = r'^\+[0-9]{2,3} [0-9]{10}$'
+    search = re.fullmatch(pattern, mobile_number)
+    if search:
+        logging.info(f"{mobile_number} is valid mobile number")
+        return True
+    else:
+        logging.warning(f"{mobile_number} is a invalid mobile number")
+        return False
+    
+def validate_password(pass_rule_1):
+    """
+	Description:
+		Checks whether password is minimum of 8 characters or not
+	Parameters:
+		pass_rule_1: password to validate
+	Return:
+		bool: True if password is valid, False otherwise
+    """
     pattern = r'^.{8,}$'
+    search = re.fullmatch(pattern, pass_rule_1)
+    if search:
+        logging.info(f"{pass_rule_1} is a valid password")
+        return True
+    else:
+        logging.warning(f"{pass_rule_1} is a invalid password")
+        return False
+            
+def main():
+    """
+	Description:
+		Collects user input and validates using necessary sub-functions.
+	Parameters:
+		None
+	Return:
+		None
+    """
+    while True:
+        first = input("Enter your first name: ")
+        if validate_first_name(first):
+            break
+
+    while True:
+        last = input("Enter your last name: ")
+        if validate_last_name(last):
+            break
+        
+    while True:
+        email_id = input("Enter your email id: ")
+        if validate_email_id(email_id):
+            break
+
+    while True:
+        mobile_number = input("Enter your mobile number (with country code): ")
+        if validate_mobile_number(mobile_number):
+            break
+        
     while True:
         password_rule_1 = input("Enter your password (min 8 characters): ")
-        search = re.fullmatch(pattern, password_rule_1)
-        if search:
-            logging.info(f"{password_rule_1} is a valid password")
-            print(f"{password_rule_1} is a valid password")
-            return password_rule_1
-        else:
-            logging.warning(f"{password_rule_1} is a invalid password")
-            print("Please enter a valid password (min 8 characters)")
-
-def mobile_number():
-    pattern = r'^91 [0-9]{10}$'
-    while True:
-        mobile_no = input("Enter your mobile number: ")
-        search = re.fullmatch(pattern, mobile_no)
-        if search:
-            logging.info(f"{mobile_no} is valid mobile number")
-            return mobile_no
-        else:
-            logging.warning(f"{mobile_no} is a invalid mobile number")
-            print("Please enter a valid mobile number")
-            
-def email_id():
-    pattern = r'^[a-zA-Z0-9+%_-]+(?:\.[a-zA-Z0-9+%_-]+)?@[a-zA-Z0-9]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$'
-    while True:
-        email = input("Enter your email id: ")
-        search = re.fullmatch(pattern, email)
-        if search:
-            logging.info(f"{email} is valid email id")
-            return email
-        else:
-            logging.warning(f"{email} is invalid email id")
-            print("Please enter a valid email id")
-
-def last_name():
-    pattern = r'^[A-Z][a-z]{2,}$'
-    while True:
-        valid_last_name = input("Enter your last name: ")
-        search = re.fullmatch(pattern,valid_last_name)
-        if search:
-            logging.info(f"{valid_last_name} is a valid last name")
-            return valid_last_name
-        else:
-            logging.warning(f"{valid_last_name} is a invalid last name. Please enter a valid last name")
-            print("Please enter valid last name")
-
-def first_name():
-    pattern = r'^[A-Z][a-z]{2,}(?: [A-Z][a-z]+){0,2}$'
-    while True:
-        valid_first_name = input("Enter your first name: ")
-        search = re.fullmatch(pattern,valid_first_name)
-        if search:
-            logging.info(f"{valid_first_name} is a valid first name")
-            return valid_first_name
-        else:
-            logging.warning(f"{valid_first_name} is a invalid first name. Please enter a valid first name")
-            print("Please enter valid first name")
+        if validate_password(password_rule_1):
+            print(f"{password_rule_1} is a valid mobile number")
+            break
+        print("Please enter a valid password (min 8 characters)")
 
 if __name__ == "__main__":
-    first_name()
-    last_name()
-    email_id()
-    mobile_number()
-    password()
+    main()           
