e99adbddeb9c85a30ea69d739319df67c452f8a2
Your Name <nikhilvardhanraju@gmail.com>
2025-02-17 00:27:18 +0530

    [Nikhil] Refactor: Maintained code hygiene and refactored the code.

--- user.py
+++ user.py
@@ -10,31 +10,63 @@ logging.basicConfig(
 		logging.StreamHandler()
 	])
 
-def last_name():
-    pattern = r'^[A-Z][a-z]{2,}$'
-    while True:
-        last_name = input("Enter your last name: ")
-        search = re.fullmatch(pattern,last_name)
-        if search:
-            logging.info(f"{last_name} is a valid last name")
-            print(f"{last_name} is a valid last name")
-            return last_name
-        else:
-            logging.warning(f"{last_name} is a invalid last name. Please enter a valid last name")
-            print("Please enter  valid last name")
-
-def first_name():
+def validate_first_name(first_name):
+    """
+	Description:
+		Checks whether first name is valid or not
+	Parameters:
+		first_name: first name to validate
+	Return:
+		bool: True if first name is valid, False otherwise
+    """
     pattern = r'^[A-Z][a-z]{2,}(?: [A-Z][a-z]+){0,2}$'
+    search = re.fullmatch(pattern,first_name)
+    if search:
+        logging.info(f"{first_name} is a valid first name")
+        return True
+    else:
+        logging.warning(f"{first_name} is an invalid first name.")
+        return False
+    
+def last_name(last_name):
+    """
+	Description:
+		Checks whether last name is valid or not
+	Parameters:
+		last_name: last name to validate
+	Return:
+		bool: True if last name is valid, False otherwise
+    """
+    pattern = r'^[A-Z][a-z]{2,}$'
+    last_name = input("Enter your last name: ")
+    search = re.fullmatch(pattern,last_name)
+    if search:
+        logging.info(f"{last_name} is a valid last name")
+        return True
+    else:
+        logging.warning(f"{last_name} is an invalid last name.")
+        return False
+            
+def main():
+    """
+	Description:
+		Collects user input and validates using necessary sub-functions.
+	Parameters:
+		None
+	Return:
+		None
+    """
     while True:
-        first_name = input("Enter your first name: ")
-        search = re.fullmatch(pattern,first_name)
-        if search:
-            logging.info(f"{first_name} is a valid first name")
-            return first_name
-        else:
-            logging.warning(f"{first_name} is a invalid first name. Please enter a valid first name")
-            print("Please enter  valid first name")
+        first = input("Enter your first name: ")
+        if validate_first_name(first):
+            break
 
+    while True:
+        last = input("Enter your last name: ")
+        if validate_first_name(last):
+            print(f"{last} is a valid last name")
+            break
+        print("Please enter valid last name")
+        
 if __name__ == "__main__":
-    first_name()
-    last_name()
+    main()
